52a91653444c0b76cfc18479139c6a3521c18429
christopher james <chriisjms@gmail.com>
2026-04-03 10:22:14 +0200

    debug admin db error

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,7 @@
-<?php
+ <?php
+  var_dump(file_exists(__DIR__ . '/../shared/bootstrap.php'));
+  var_dump(__DIR__);
+  die();
 require_once __DIR__ . '/../shared/bootstrap.php';
 /**
  * Admin Dashboard
